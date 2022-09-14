e6ff63f53cc6d4b6582a4be20831cd12404a70b5
Anthony Blackwell Tallent <anthonytallent567@gmail.com>
2022-09-14 12:27:31 -0700

    Finish this practice

--- friends.rb
+++ friends.rb
@@ -4,3 +4,10 @@ arrays = ["Henry", "Fernando", "Jessie", "Sammy", "Fuaad"]
 puts arrays[2]
 puts arrays[4]
 puts arrays[0]
+
+
+places = ["Hawaii", "Alleyway", "Vons", "Amsterdam", "London"]
+
+places.insert(3, "Taco Bell")
+
+puts places[3]
